d0da50d12408cfaae3a8329d28116d840ba2901b
rounak <rounakmalik168@gmail.com>
2026-02-23 16:49:34 +0530

    added docker file and docker compse

--- database_connection.py
+++ database_connection.py
@@ -1,10 +1,12 @@
 import os
 import psycopg2
-import load
+from dotenv import load_dotenv
+
+load_dotenv()  
 
 conn = psycopg2.connect(
     dbname=os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST")
+    host=os.getenv("DB_HOST", "db") 
 )
---
+++ dockerfile
@@ -0,0 +1,15 @@
+# Base image
+FROM python:3.11-slim
+
+# Set working directory
+WORKDIR /app
+
+# Copy requirements and install
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy app code
+COPY . .
+
+# Run your app
+CMD ["python", "main.py"]

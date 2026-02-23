0bcc5f1eb96a425abe7f8eed656f6ad5852e777e
rounak <rounakmalik168@gmail.com>
2026-02-23 16:46:06 +0530

    changes for ignore file and evn variables

--- database_connection.py
+++ database_connection.py
@@ -1,17 +1,10 @@
-import psycopg2 
+import os
+import psycopg2
+import load
 
 conn = psycopg2.connect(
-    
-    database = "postgres",
-    user = "postgres",
-    password = "pass123",
-    host = "localhost",
-    port = 5432
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST")
 )
-
-print("connected successfully")
-
-
-
-
-
